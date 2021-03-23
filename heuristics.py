3a04939c6341864fe45758199122f6bb99160008
kamilgeagea <kamilgeagea8199@gmail.com>
2021-03-22 20:25:24 -0400

    implemented 2nd heuristic

--- heuristics.py
+++ heuristics.py
@@ -1,3 +1,4 @@
+import numpy as np
 from utility_functions import generate_goal, get_coords
 
 '''
@@ -26,5 +27,38 @@ def h1(state):
             (goal_i, goal_j) = get_coords(goal, element)
 
             sum += abs(i - goal_i) + abs(j - goal_j)
+
+    return sum
+
+
+'''
+Heuristic that calculates the sum of the sum of the euclidian distance between an element and its goal position
+Arguments:
+  - state: NxN Matrix
+Returns: Number -> Sum of each element minimum moves needed to get to its goal coords
+'''
+
+
+def h2(state):
+    # Get Matrix Dimensions
+    dimensions = len(state)
+
+    # Generate Goal
+    goal = generate_goal(dimensions)
+
+    # Initiate sum
+    sum = 0
+
+    # Loop through every element and compute distance with the goal coords
+    # Add the distance to the sum
+    for i in range(0, dimensions):
+        for j in range(0, dimensions):
+            element = state[i][j]
+            goal_element = get_coords(goal, element)
+
+            point1 = np.array(goal_element)
+            point2 = np.array((i, j))
+
+            sum += np.linalg.norm(point1-point2)
 
     return sum
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dfs import depth_first_search
 from id import iterative_deepening
 from a_star import a_star
-from heuristics import h1
+from heuristics import h1, h2
 from utility_functions import generate_puzzle
 
 print("\n\n")
@@ -10,51 +10,56 @@ print("\n\n")
 
 puzzle2x2 = generate_puzzle(2)
 
-# Apply Depth-First-Search on 2x2 puzzle
-dfs2x2 = depth_first_search(puzzle2x2, k=None)
-print("===== Depth-First-Search - 2x2 Matrix =====\n")
-print("Path: " + str(dfs2x2["data"]))
-print("Execution time - " + str(dfs2x2["execution_time"]) + " seconds\n\n")
+# # Apply Depth-First-Search on 2x2 puzzle
+# dfs2x2 = depth_first_search(puzzle2x2, k=None)
+# print("===== Depth-First-Search - 2x2 Matrix =====\n")
+# print("Path: " + str(dfs2x2["data"]))
+# print("Execution time - " + str(dfs2x2["execution_time"]) + " seconds\n\n")
 
-# Apply Iterative Deepening on 2x2 puzzle
-id2x2 = iterative_deepening(puzzle2x2)
-print("===== Iterarive-Deepening - 2x2 Matrix =====\n")
-print("Path: " + str(id2x2["data"]))
-print("Execution time - " + str(id2x2["execution_time"]) + " seconds\n\n")
+# # Apply Iterative Deepening on 2x2 puzzle
+# id2x2 = iterative_deepening(puzzle2x2)
+# print("===== Iterarive-Deepening - 2x2 Matrix =====\n")
+# print("Path: " + str(id2x2["data"]))
+# print("Execution time - " + str(id2x2["execution_time"]) + " seconds\n\n")
 
-# Apply A* Algorithm on 2x2 puzzle
-as2x2 = a_star(puzzle2x2, h1)
-print("===== A-Star - 2x2 Matrix =====\n")
-print("Path: " + str(as2x2["data"]))
-print("Execution time - " + str(as2x2["execution_time"]) + " seconds\n\n")
+# # Apply A* Algorithm on 2x2 puzzle
+# as2x2 = a_star(puzzle2x2, h1)
+# print("===== A-Star - 2x2 Matrix =====\n")
+# print("Path: " + str(as2x2["data"]))
+# print("Execution time - " + str(as2x2["execution_time"]) + " seconds\n\n")
 
 # Create 3x3 puzzle
 puzzle3x3 = generate_puzzle(3)
 
-# Apply Depth-First-Search on 3x3 puzzle
-dfs3x3 = depth_first_search(puzzle3x3, k=None)
-print("===== Depth-First-Search - 3x3 Matrix =====\n")
-print("Path: " + str(dfs3x3["data"]))
-print("Execution time - " + str(dfs3x3["execution_time"]) + " seconds\n\n")
+# # Apply Depth-First-Search on 3x3 puzzle
+# dfs3x3 = depth_first_search(puzzle3x3, k=None)
+# print("===== Depth-First-Search - 3x3 Matrix =====\n")
+# print("Path: " + str(dfs3x3["data"]))
+# print("Execution time - " + str(dfs3x3["execution_time"]) + " seconds\n\n")
 
-# Apply Iterative Deepening on 3x3 puzzle
-id3x3 = iterative_deepening(puzzle3x3)
-print("===== Iterarive-Deepening - 3x3 Matrix =====\n")
-print("Path: " + str(id3x3["data"]))
-print("Execution time - " + str(id3x3["execution_time"]) + " seconds\n\n")
+# # Apply Iterative Deepening on 3x3 puzzle
+# id3x3 = iterative_deepening(puzzle3x3)
+# print("===== Iterarive-Deepening - 3x3 Matrix =====\n")
+# print("Path: " + str(id3x3["data"]))
+# print("Execution time - " + str(id3x3["execution_time"]) + " seconds\n\n")
 
-# Apply A* Algorithm on 3x3 puzzle
+# # Apply A* Algorithm on 3x3 puzzle
 as3x3 = a_star(puzzle3x3, h1)
 print("===== A-Star - 3x3 Matrix =====\n")
 print("Path: " + str(as3x3["data"]))
-print("Execution time - " + str(as3x3["execution_time"]) + " seconds\n\n")
+print("Execution time h1 - " + str(as3x3["execution_time"]) + " seconds\n\n")
 
 
-# Create 5x5 puzzle
-puzzle5x5 = generate_puzzle(5)
+# # Create 5x5 puzzle
+# puzzle5x5 = generate_puzzle(5)
 
-# Apply A* Algorithm on 5x5 puzzle
-as5x5 = a_star(puzzle5x5, h1)
-print("===== A-Star - 4x4 Matrix =====\n")
-print("Path: " + str(as5x5["data"]))
-print("Execution time - " + str(as5x5["execution_time"]) + " seconds\n\n")
+# # Apply A* Algorithm on 5x5 puzzle
+# as5x5 = a_star(puzzle5x5, h1)
+# print("===== A-Star - 4x4 Matrix =====\n")
+# print("Path: " + str(as5x5["data"]))
+# print("Execution time - " + str(as5x5["execution_time"]) + " seconds\n\n")
+
+as3x3 = a_star(puzzle3x3, h2)
+print("===== A-Star - 3x3 Matrix =====\n")
+print("Path: " + str(as3x3["data"]))
+print("Execution time h2 - " + str(as3x3["execution_time"]) + " seconds\n\n")
